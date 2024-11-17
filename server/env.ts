0967f89d832073d0b0c80384ed57a2811d564134
Kyle Pena <kylepena@Kyles-MacBook-Air.local>
2024-11-16 23:18:31 -0500

    some basic (temporary) pub sub introduced into index.ts

---
+++ server/env.ts
@@ -0,0 +1,6 @@
+
+
+export const env = {
+    PORT: Number(process.env.PORT) || 3000,
+    NATS_SERVER_PORT: Number(process.env.NATS_SERVER_PORT) || 4223
+};
--- server/index.ts
+++ server/index.ts
@@ -1,17 +1,47 @@
 import { z, ZodType } from "zod";
-import { Hono } from "hono";
+import { Hono, type Context } from "hono";
+import { env } from "./env.ts";
+import { connect, StringCodec } from "nats";
+
+const nc = await connect({ servers: `https://localhost:${env.NATS_SERVER_PORT}`});
+const sc = StringCodec();
+const sub = nc.subscribe("hello");
+
+
+(async () => {
+  for await (const m of sub) {
+    console.log(`[${sub.getProcessed()}]: ${sc.decode(m.data)}`);
+  }
+  console.log("subscription closed");
+})();
+
+nc.publish("hello", sc.encode("hello"));
+nc.publish("hello", sc.encode("world2"));
+
+await nc.drain();
 
 const app = new Hono();
 
-app.get('/imagePrompt', async (c) => {
+app.get('/imageGenPrompt', async (c) => {
   // applesauce
+  const prompt = await readImageGenPromptFromRequest(c);
   const response = await fetch("http://localhost:8222/routez");
-  return c.text(await response.text());
+  return c.text(prompt + " " + (await response.text()));  
 });
 
-console.log("Starting server on port 3000");
+app.post('/registerWorker', async (c) => {
+
+});
+
+// TODO: schema validation
+async function readImageGenPromptFromRequest(c : Context) : Promise<string> {
+  const request = (await c.req.json().catch(e => null)) || c.req.query()
+  return request.prompt;
+}
+
+console.log(`Starting server on port ${env.PORT}`);
 
 export default {
-  port: 3000,
+  port: env.PORT,
   fetch: app.fetch
 };
