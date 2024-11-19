5774c15a1906f3f95dc2b84df8a5b3f053cc96f2
Kyle Pena <kylepena@Kyles-MacBook-Air.local>
2024-11-19 18:57:52 -0500

    fixed docker compose and updated init sql

--- db/init.sql
+++ db/init.sql
@@ -1,3 +1,30 @@
-CREATE TABLE dummy (
-    id SERIAL PRIMARY KEY
-)
+-- CreateTable
+CREATE TABLE "ImgGenRequest" (
+    "id" SERIAL NOT NULL,
+    "imageInbox" TEXT NOT NULL,
+    "prompt" TEXT NOT NULL,
+    "seed" INTEGER NOT NULL,
+    "numSteps" INTEGER NOT NULL,
+    "height" INTEGER NOT NULL,
+    "width" INTEGER NOT NULL,
+    "workerId" TEXT,
+    "start" TIMESTAMP(3),
+    "end" TIMESTAMP(3),
+    "successful" BOOLEAN,
+    "passesVerification" BOOLEAN,
+    "created" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "ImgGenRequest_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "BlacklistedWorker" (
+    "id" SERIAL NOT NULL,
+    "workerID" TEXT NOT NULL,
+
+    CONSTRAINT "BlacklistedWorker_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ImgGenRequest_imageInbox_key" ON "ImgGenRequest"("imageInbox");
