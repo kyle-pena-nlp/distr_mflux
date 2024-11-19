fc6cf03ba35094773b2a6a38284e43f56c893ecb
Kyle Pena <kylepena@Kyles-MacBook-Air.local>
2024-11-18 20:59:14 -0500

    incorporated prisma schema

--- server/env.ts
+++ server/env.ts
@@ -1,4 +1,4 @@
 export const env = {
-    PORT: Number(process.env.PORT) || 3000,
-    NATS_SERVER_URL: (process.env.NATS_SERVER_URL || '').trim() || `http://nats:4222`
+    PORT: Number(process.env.PORT),
+    NATS_SERVER_URL: (process.env.NATS_SERVER_URL || '').trim()
 };
